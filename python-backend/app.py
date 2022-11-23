e1239a78a89b1f41b72dffb5a47c3c4a424cc4e1
rounaksalim95 <rounaksalim@gmail.com>
2022-11-23 11:57:35 -0800

    Get basic model for User working

---
+++ python-backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify, make_response
+from models import database, user
+
+app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://test:password@localhost/cleaning"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:password@localhost:5432/cleaning"
+
+database.db.init_app(app)
+
+with app.app_context():
+    database.db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ python-backend/models/user.py
@@ -0,0 +1,11 @@
+from .database import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    public_id = db.Column(db.String, unique=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    user_type = db.Column(db.String, nullable=False)
+    address = db.Column(db.String)
+    phone = db.Column(db.String)
